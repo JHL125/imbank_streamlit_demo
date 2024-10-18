414a1923264dbf4fcf61a5feb0d6d91e5581c347
JH125 <JHL125>
2024-10-18 09:43:56 +0900

    updated

--- app.py
+++ app.py
@@ -1,10 +1,18 @@
 # -*- coding:utf-8 -*-
 import streamlit as st 
+import pandas as pd 
 
+@st.cache_data
+def load_data():
+    train = pd.read_csv("dataset/train.csv")
+    return train
 
 def main():
     st.title("Hello World!")
     st.title("Hello World!!")
+
+    train = load_data()
+    st.dataframe(train)
 
 if __name__ == "__main__":
     main()
